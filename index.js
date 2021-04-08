6351ae510c03b90c1724620d4f623ec1fa3a9f5a
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-08 16:45:04 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+   return drivers.filter(match => match.toUpperCase() === name.toUpperCase())}
+
+   function fuzzyMatch(drivers, string){
+    return drivers.filter(match => match.startsWith(string))
+}
+
+function matchName (drivers, name){
+    return drivers.filter(match => match.name === 'Bobby')
+}
+   
